04fc35629d13a6c352764cdf8253d2206a917b4c
marcosDLCS <marcos.dlcs@gmail.com>
2017-08-08 19:43:57 +0200

    Course list view. New course view. Changes

--- src/pages/course-list/course-list.ts
+++ src/pages/course-list/course-list.ts
@@ -15,8 +15,18 @@ export class CourseList {
     this.http.get("http://localhost:8081/course-catalog/api/courses")
       .map(res => res.json()).subscribe(data => {
       this.courses = data;
-      console.log(this.courses)
+      console.log("This is my response: " + JSON.stringify(this.courses));
     });
   }
 
+  badgeColor(level: String) {
+    let color: String = "";
+    if (level.toLowerCase() == 'basico'){
+      color = 'secondary';
+    }
+    if (level.toLowerCase() == 'avanzado'){
+      color = 'danger'
+    }
+    return color;
+  }
 }
--- src/pages/new-course/new-course.ts
+++ src/pages/new-course/new-course.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,26 @@ import { NavController } from 'ionic-angular';
 })
 export class NewCourse {
 
-  constructor(public navCtrl: NavController) {
+  private newForm : FormGroup;
+  teachers:any;
 
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder, private http : Http ) {
+    this.newForm = this.formBuilder.group({
+      active: ['true', Validators.required],
+      teacher: ['', Validators.required],
+      name: ['', Validators.required],
+      level: ['', Validators.required],
+      hours: ['', Validators.required]
+    });
+    this.http.get("http://localhost:8081/course-catalog/api/teachers")
+      .map(res => res.json()).subscribe(data => {
+      this.teachers = data;
+      console.log("This is my response - [teachers] : " + JSON.stringify(this.teachers));
+    });
+  }
+
+  logForm(){
+    console.log(this.newForm.value)
   }
 
 }
